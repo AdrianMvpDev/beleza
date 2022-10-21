25e3a5fac8543f77310b454b12e2f8c54506ba39
AdrianMvpDev <91435144+AdrianMvpDev@users.noreply.github.com>
2022-10-21 05:36:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Belleza infinita</title>
+  <link rel="stylesheet" href="aplicaciones/css/inicio.css">
+  <link rel="stylesheet" href="aplicaciones/css/components/header.css">
+  <link rel="stylesheet" href="aplicaciones/css/components/footer.css">
+  <link rel="stylesheet" href="aplicaciones/css/pages/home.css">
+  <link rel="stylesheet" href="aplicaciones/css/slider.css">
+</head>
+
+<body>
+    <header>
+      <h1 class="nav-title">Belleza infinita</h1>
+      <nav>
+        <ul class="nav-container">
+          <li><a href="/" class="nav-list">Inicio</a></li>
+          <li><a href="vistas/rostro.php" class="nav-list">Rostro</a></li>
+          <li><a href="vistas/catalogo.php" class="nav-list">Catalogo</a></li>
+          <li><a href="vistas/maquillaje.php" class="nav-list">Maquillaje</a></li>
+          <li><a href="vistas/formulariologin.php" class="nav-list">Inicio Sesión </a></i></li>
+        </ul>
+      </nav>
+    </header>
+
+  <!--Contenido Central-->
+
+  <div id="kiko-slider">
+
+    <ul class="slider">
+      <li id="slide1">
+        <picture>
+          <img src="aplicaciones/img/hdd.jpg" />
+        </picture>
+      </li>
+      <li id="slide2">
+        <img src="aplicaciones/img/mate.jpg" />
+      </li>
+      <li id="slide3">
+        <img src="aplicaciones/img/ceja.jpg" />
+      </li>
+    </ul>
+
+    <ul class="menu">
+      <li>
+        <a href="#slide1"></a>
+      </li>
+      <li>
+        <a href="#slide2"></a>
+      </li>
+      <li>
+        <a href="#slide3"></a>
+      </li>
+    </ul>
+
+  </div>
+    <div class="card_container" >
+        <div class= "container">
+          <div class= "card">
+            <div class="card-img">
+              <img src="aplicaciones/img/pinta.jpg">
+            </div class="card-img">
+            <h4>Labios</h4>
+
+            <p>La forma correcta para maquillar tus labios Para lograrlo, toma un delineador en tono piel
+            (Un tono muy suave) y realiza un trazado ligeramente en el labio superior (delinea un poco más allá de las comisuras) y ensancha el inferior 
+            (delinea un poquito más abajo, sobre todo en la parte media). Después toma el labial y rellena los espacios con una brocha.</p>        
+          </div>
+        </div>
+
+        <div class= "container">
+          <div class= "card">
+              <div class="card-img">
+                <img src="aplicaciones/img/ojito.jpg">
+              </div class="card-img">
+              <h4>Ojos</h4>
+
+              <p>¿Cómo maquillarse los ojos de acuerdo a su forma?
+              Resultado de imagen para forma correcta de maquillar los ojos
+              Aplica un tono claro sobre el lagrimal y el hueso de la ceja para dar luz a la mirada. 
+              Seguiremos con un tono intermedio, el cual aplicaremos en el párpado y en la línea de las pestañas. Por último,utilizaremos 
+              el tono más oscuro en el tercio externo del ojo en forma de V girada. Difumínala bien hacia afuera.</p>
+          </div>
+        </div>
+
+        <div class= "container">
+          <div class= "card">
+            <div class="card-img">
+              <img src="aplicaciones/img/cejita.jpg">
+            </div class="card-img">
+            <h4>Cejas</h4>
+              
+            <p>¿Cómo se usan las cejas 2022?
+            Resultado de imagen para forma correcta de maquillar las cejas
+            Las cejas naturales y gruesas siguen siendo una de las tendencias de maquillaje de 2022. Las famosas lo demuestra y también
+            las técnicas como el microblading, el microshading o el laminado de cejas.</p>
+          </div>
+        </div>
+  </div>
+  
+  <footer>
+      <ul class="footer-ul" >
+          <li class="footer-li"><a href="" class="footer-a">sayra8@gmail.com</a></li>
+          <li class="footer-li"><a href="" class="footer-a" >celular: 3122447895</a></li>
+      </ul>
+      <ul class="footer-h4">   
+        <li>Derechos de autor google</li>
+        <li>Desarrollado por: Sayra Jelanny Segura Contreras</li>
+        <li>Bogotá 2022</li>    
+      </ul>
+    </footer>
+  </body>
+</html>
